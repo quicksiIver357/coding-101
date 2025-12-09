eb1972b1c1242083725a1192df6910121c6a2a68
quicksiIver357 <ryanbmatar@gmail.com>
2025-12-09 21:04:17 +0000

    added a timer to see how long it took to run just quality of life stuff

--- cool math stuff/prime-graph.py
+++ cool math stuff/prime-graph.py
@@ -1,29 +1,62 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from time import perf_counter
+from datetime import timedelta
 
-def is_prime(n):
-    if n < 2:
-        return False
-    if n == 2:
-        return True
-    if n % 2 == 0:
-        return False
-    for i in range(3, int(n**0.5) + 1, 2):
-        if n % i == 0:
-            return False
-    return True
 
-def find_first_x_primes(x):
-    primes = []
-    n = 2
-    while len(primes) < x:
-        if is_prime(n):
-            primes.append(n)
-        n += 1
-    return primes
+def sieve_of_eratosthenes_optimized(limit):
+    """
+    Generates all prime numbers up to 'limit' using an optimized 
+    Sieve of Eratosthenes, utilizing numpy for speed.
+    """
+    if limit < 2:
+        return []
+    
+    # We only need to store odd numbers from 3 onwards
+    # Index i corresponds to number 2*i + 3
+    size = (limit + 1) // 2
+    is_prime = np.ones(size, dtype=bool)
+    
+    # The first odd number is 3, start p from 3
+    p = 3
+    while p * p <= limit:
+        # If is_prime[i] is True, then p is prime
+        if is_prime[(p - 3) // 2]:
+            # Mark multiples of p as non-prime. 
+            # Start from p*p (lower multiples were already marked by smaller primes)
+            # and increment by 2*p to only mark odd multiples.
+            start_index = (p * p - 3) // 2
+            # Use slicing for performance
+            is_prime[start_index::p] = False
+        
+        # Move to the next odd number
+        p += 2
+        
+    # Collect the primes
+    primes = [2]
+    # Use numpy where to quickly get indices of True values
+    odd_primes = 2 * np.where(is_prime)[0] + 3
+    primes.extend(odd_primes)
+    
+    # Filter out any primes that might exceed the limit due to array sizing
+    return [p for p in primes if p <= limit]
+
+
 
 # Find first 50 primes
 x = int(input("Enter the number of prime numbers to plot: "))
-primes = find_first_x_primes(x)
+
+start_time = perf_counter() # start timer
+
+primes = sieve_of_eratosthenes_optimized(x)
+
+end_time = perf_counter() # end timer
+
+
+elapsed_secs = end_time - start_time # calculate elapsed time
+
+duration = timedelta(seconds=elapsed_secs) # format elapsed time
+print(f"Found {len(primes)} primes up to {x} in {duration}. Graph saved as 'prime_graph.png'.") # print results
 
 # Create y-values (index + 1 for each prime)
 y_values = list(range(1, len(primes) + 1))
